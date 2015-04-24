126958a1d72d79fb32e7a3fec01fd2e870674451
C.D. Clark III <clifton.clark@gmail.com>
2015-04-24 13:11:15 -0500

    developing pyHomework module

--- src/pyHomework.py
+++ src/pyHomework.py
@@ -1,150 +1,152 @@
 #! /usr/bin/env python
 
-from Utils import *
-import sys, os, re, random
-import math
-import yaml
-import subprocess, tempfile, shutil, shlex
+import sys, os, subprocess, shlex
+
+import sympy as sy
+import numpy as np
+import pint  as pn
+
 from wheezy.template.engine import Engine
 from wheezy.template.ext.core import CoreExtension
 from wheezy.template.loader import DictLoader
 
+units = pn.UnitRegistry()
+
+class HomeworkAssignment:
 
 
-latex_template= r'''
-@require(metadata,questions)
+
+  def __init__(self):
+    self.latex_template= r'''
+@require(config)
 \documentclass[letterpaper,10pt]{article}
 \usepackage{amsmath}
 \usepackage{amsfonts}
 \usepackage{amssymb}
 \usepackage{graphicx}
-\usepackage{siunitx}
+\usepackage[per-mode=symbol]{siunitx}
 \usepackage[left=1in,right=1in,top=1in,bottom=1in]{geometry}
+\usepackage{fancyhdr}
+\usepackage[ampersand]{easylist}
+\ListProperties(Numbers1=a,Numbers2=l,Hide2=1,Progressive*=0.5cm,Hang=true,Space2=0.2cm,Space1=0.4cm,Space1*=0.4cm)
 
-\title{@metadata['class']['name']}
+\setlength{\headheight}{0.5in}
+\pagestyle{fancyplain}
+\fancyhead[L]{@config['LH']}
+\fancyhead[C]{@config['CH']}
+\fancyhead[R]{@config['RH']}
+\fancyfoot[L]{@config['LF']}
+\fancyfoot[C]{@config['CF']}
+\fancyfoot[R]{@config['RF']}
+\renewcommand{\headrulewidth}{0pt}
+
+\setlength{\parindent}{0cm}
+
+\title{@config['title']}
+\author{}
+\date{}
 
 \begin{document}
 \maketitle
 
-\begin{enumerate}
-@for question in questions:
-    \item @question['tex']
+@for q in config['questions']:
+\begin{minipage}{\linewidth}
+\begin{easylist}
+  & @q['star'] @q['text']
+    @if 'parts' in q:
+      @for p in q['parts']:
+      && @p['star'] @p['text']
+      @end
+    @end
+\end{easylist}
+\end{minipage}
 @end
-\end{enumerate}
+
+
+\clearpage
+
+@for f in config['figures']:
+\begin{figure}
+\includegraphics[@f['options']]{@f['filename']}
+\caption{ \label{@f['label']} @f['caption']}
+\end{figure}
+@end
 
 \end{document}
 '''
+    self.config = { 'title' : "UNKNOWN"
+                  , 'LH' : ""
+                  , 'CH' : ""
+                  , 'RH' : ""
+                  , 'LF' : ""
+                  , 'CF' : r"\thepage"
+                  , 'RF' : r"powered by \LaTeX"
+                  , 'questions' : [ ]
+                  , 'figures' : [ ]
+                  }
+
+    self.questions = self.config['questions']
+    self.figures   = self.config['figures']
+
+    self.blank_question = {'text' : "", 'star' : "" }
+    self.blank_part     = {'text' : "", 'star' : "" }
+    self.blank_figure   = {'filename' : "", 'caption' : "", 'label' : "", 'options' : "" }
 
 
-class ProblemSet:
-    def __init__(self):
-        self.quiz_data = None
-        self.quiz_namespace = None
-        self.correct_answer_chars = "*^!@"
-        self.randomize_answers = True
-        self.show_answers      = True
-        self.questions_answers = True
-        self.template_engine = Engine( loader=DictLoader( {'doc': latex_template} )
-                                     , extensions=[CoreExtension()] )
 
-        self.template_engine.global_vars.update({'mca' : self.filter_multiple_choice_answer})
-        self.template_engine.global_vars.update({'saa' : self.filter_short_answer__answer})
-        self.template_engine.global_vars.update({ 'fa' : self.filter_formula_answer})
-        self.template_engine.global_vars.update({ 'na' : self.filter_numerical_answer})
-        
+    self.template_engine = Engine( loader=DictLoader( {'doc': self.latex_template} ) , extensions=[CoreExtension()] )
 
-    def load(self, obj):
-        if isinstance( obj, str ):
-            if os.path.isfile( obj ):
-                with open(obj) as f:
-                    self.quiz_data = yaml.load( f )
-            else:
-                raise IOError( "argument %s does not seem to be a file" % arg )
+  def write_latex(self, filename=None):
+    if not filename:
+        filename = "/dev/stdout"
+    with open(filename, 'w') as f:
+      f.write( self.template_engine.get_template("doc").render( {'config': self.config} ) )
 
-        if isinstance( obj, dict ):
-            self.quiz_data = obj
+  def build_PDF( self, filename=None):
+    if not filename:
+      filename = "main.pdf"
 
-        for i in range(len(self.quiz_data['questions'])):
-            self.quiz_data['questions'][i]['id'] = random.randint(0,100000)
+    basename = os.path.splitext(filename)[0]
 
-        self.namespace = { 'global_vars' : self.quiz_data.get('vars',{} ) }
-        self.namespace['global_vars']['local_vars'] = []
-        for i in range(len(self.quiz_data['questions'])):
-            self.namespace['global_vars']['local_vars'].append( self.quiz_data['questions'][i].get('vars',{}) )
-            
-        self.interpolate()
-        self.detect_question_types()
-        self.format_answers()
+    self.write_latex(basename+".tex")
 
-    def interpolate(self, tree = None, subs = None):
-        '''Interpolate variable references in entire document.
-           We interpolate all strings in the document, but only interpolate
-           from variables defined under var nodes.'''
-
-        if tree == None:
-            tree = self.quiz_data
-            subs = EvalTemplateDict( self.namespace )
-            subs.update( self.namespace['global_vars'] )
-            for k in tree:
-                if isinstance( tree[k], str ):
-                    tree[k] = EvalTemplate(tree[k]).substitute(subs)
-
-                if k != "questions":
-                    self.interpolate( tree[k], subs )
-
-            for i in range(len(self.quiz_data["questions"])):
-                question = self.quiz_data["questions"][i]
-                subs = EvalTemplateDict( self.namespace )
-                subs.update( self.namespace['global_vars']['local_vars'][i] )
-
-                self.interpolate( question, subs )
-        else:
-            if isinstance(tree, dict):
-                for k in tree:
-                    if isinstance( tree[k], str ):
-                        tree[k] = EvalTemplate(tree[k]).substitute(subs)
-                    else:
-                        self.interpolate(tree[k],subs)
-
-            elif isinstance(tree, list):
-                for i in range(len(tree)):
-                    if isinstance( tree[i], str ):
-                        tree[i] = EvalTemplate(tree[i]).substitute(subs)
-                    else:
-                        self.interpolate(tree[i],subs)
-                    
-
-                    
-    def write_questions(self, filename=None):
-        if not filename:
-            filename = "/dev/stdout"
-        with open(filename, 'w') as f:
-            f.write( self.template_engine.get_template("doc").render( self.quiz_data ) )
-
-    def compile_latex(self, filename):
-        basename = os.path.splitext(arg)[0]
-        pdf      = basename+".pdf"
-        cwd = os.getcwd()
-        temp = tempfile.mkdtemp()
-
-        
-        shutil.copy( filename, temp )
-        os.chdir( temp )
-
-        ret = subprocess.call(shlex.split( 'pdflatex --interaction=batchmode '+basename) )
-        ret = subprocess.call(shlex.split( 'pdflatex --interaction=batchmode '+basename) )
-        ret = subprocess.call(shlex.split( 'bibtex '+basename) )
-        ret = subprocess.call(shlex.split( 'pdflatex --interaction=batchmode '+basename) )
-
-        shutil.copy( pdf, cwd)
-        os.chdir( cwd)
+    with open("/dev/stdout",'w') as FNULL:
+      ret = subprocess.call(shlex.split( 'latexmk -pdf '+basename), stdout=sys.stdout, stderr=subprocess.STDOUT)
 
 
-if __name__ == "__main__":
-    for arg in sys.argv[1:]:
-        quiz = ProblemSet()
-        quiz.load( arg )
-        quiz.write_questions( os.path.splitext(arg)[0]+".tex" )
-        quiz.compile_latex( os.path.splitext(arg)[0]+".tex" )
+    
 
+
+
+
+  def add_question(self):
+    self.questions.append( self.blank_question.copy() )
+
+  def add_part(self):
+    if not 'parts' in self.questions[-1]:
+      self.questions[-1]['parts'] = list()
+    self.questions[-1]['parts'].append( self.blank_part.copy() )
+
+  def add_text(self,text=""):
+    if 'parts' in self.questions[-1]:
+      self.questions[-1]['parts'][-1]['text'] += text + " "
+    else:
+      self.questions[-1]['text']  += text + " "
+
+  def add_star(self, text="*"):
+    if 'parts' in self.questions[-1]:
+      self.questions[-1]['parts'][-1]['star'] = text
+    else:
+      self.questions[-1]['star'] = text
+
+  def add_vars(self,vars={}):
+    self.config.update( vars )
+
+  def add_figure(self,filename=""):
+    self.figures.append( self.blank_figure.copy() )
+    self.figures[-1]['filename'] = filename
+
+  def add_figure_data(self,data,text=""):
+    if len(self.figures) > 0:
+      self.figures[-1][data] = text
 
