ce8ff9558c7802cf51eb804176c69dd947bde161
C.D. Clark III <clifton.clark@gmail.com>
2015-07-06 21:51:14 -0500

    updated installer for scripts

--- Makefile
+++ Makefile
@@ -1,7 +1,2 @@
 install:
-	cp ./src/BbQuiz.py ~/install/bin
-	cp ./src/PaperQuiz.py  ~/install/bin
-	cp ./src/HR2YAML.pl ~/install/bin
-	cp ./src/txt2yamlquiz.sh ~/install/bin
-
-	cp -r ./src/pyHomework ~/install/lib64/python2.7//site-packages/
+	python setup.py install --prefix=${HOME}/install
--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup, find_packages
 import versioneer
 
 DESCRIPTION = "Homework and quiz generators"
 LONG_DESCRIPTION = open('README.md').read()
 
+scripts = [ os.path.join('scripts',f) for f in os.listdir('scripts/') if not f.startswith('.') ]
 setup(name='pyHomework',
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
@@ -16,4 +18,5 @@ setup(name='pyHomework',
       license="MIT License",
       platforms=["any"],
       packages=find_packages(),
+      scripts=scripts
      )
