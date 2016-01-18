7436aa133277285acb4ec01e1ee062165be35b93
C.D. Clark III <clifton.clark@gmail.com>
2016-01-18 15:26:19 -0600

    fixes in numpy support

--- pyHomework/__init__.py
+++ pyHomework/__init__.py
@@ -1,14 +1,20 @@
+import sys
+
 try:
   import sympy
   from .sympy import *
   from .sympy.Equations import *
-except:
+except Exception as e:
   print "sympy failed to import. sympy support disabled."
-
+  print "Error Raised:"
+  print e
+ 
 try:
   from .numpy.quantity_calcs import *
-except:
+except Exception as e:
   print "numpy failed to import. numpy support disabled."
+  print "Error Raised:"
+  print e
 
 from .Constants import *
 from .HomeworkAssignment import *
--- pyHomework/numpy/quantity_calcs.py
+++ pyHomework/numpy/quantity_calcs.py
@@ -3,33 +3,33 @@ Unit support for numpy array calculations.
 '''
 
 import numpy as np
-import pyErrorProp as ep
+from pyErrorProp import Q_
 
 def unitof(q):
-  if isinstance(q,eq.Q_):
+  if isinstance(q,Q_):
     return q.units
   else:
-    return eq.Q_("dimensionless")
+    return Q_("dimensionless")
 
 def dot(v1,v2):
   u1 = unitof( v1 )
   u2 = unitof( v2 )
   w  = v1.dot(v2)
-  return w*eq.Q_(1,u1)*eq.Q_(1,u2)
+  return w*Q_(1,u1)*Q_(1,u2)
 
 def cross(v1,v2):
   u1 = unitof( v1 )
   u2 = unitof( v2 )
   w  = np.cross(v1,v2)
-  return w*eq.Q_(1,u1)*eq.Q_(1,u2)
+  return w*Q_(1,u1)*Q_(1,u2)
 
 def magnitude( vec ):
   return np.sqrt( dot( vec, vec ) )
 
 def direction( vec ):
   ret = np.arctan2( vec[1], vec[0] )
-  if ret < 0*units.radian:
-    ret += 2*3.14159*units.radian
+  if ret < Q_(0,'radian'):
+    ret += Q_(2*3.14159,'radian')
   return ret
 
 null = np.array( [0,0,0] )
