e11366769328023fe334faae23a93357e59bea51
C.D. Clark III <clifton.clark@gmail.com>
2016-02-07 07:42:12 -0600

    cleaned up unit support in vector calcs

--- pyHomework/numpy/quantity_calcs.py
+++ pyHomework/numpy/quantity_calcs.py
@@ -9,7 +9,7 @@ def unitof(q):
   if isinstance(q,Q_):
     return q.units
   else:
-    return Q_("dimensionless")
+    return Q_(1,"dimensionless").units
 
 def magof(q):
   if isinstance(q,Q_):
@@ -18,10 +18,8 @@ def magof(q):
     return q
 
 def dot(v1,v2):
-  u1 = unitof( v1 )
-  u2 = unitof( v2 )
-  w  = v1.dot(v2)
-  return w*Q_(1,u1)*Q_(1,u2)
+  w = sum( [ x[0]*x[1] for x in zip(v1,v2) ] )
+  return w
 
 def cross(v1,v2):
   u1 = unitof( v1 )
